59b5714e56b0ab1d4c927676c677f38881ef3e2b
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-07 13:57:48 -0600

    Initialize clean CRA app for boilerplate

---
+++ README.md
@@ -0,0 +1,12 @@
+# capacitor-react-app
+
+A boilerplate for building PWA mobile apps with Capacitor.js and React
+
+### Setup
+
+```
+$ git clone git@github.com:dhruv-m-patel/capacitor-react-app.git
+$ cd capacitor-pwa-app
+$ npm i
+$ npm start
+```
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="root">
+      <h1>Welcome to Capacitor React App</h1>
+      <p>A boilerplate for building PWA mobile apps with Capacitor.js and React</p>
+    </div>
+  );
+}
