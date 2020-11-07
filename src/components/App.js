04fcce4e1b89cd5e4192ca1938cc03362222dc66
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-07 14:16:25 -0600

    Cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
 
 export default function App() {
   return (
-    <div className="root">
+    <div>
       <h1>Welcome to Capacitor React App</h1>
       <p>A boilerplate for building PWA mobile apps with Capacitor.js and React</p>
     </div>
